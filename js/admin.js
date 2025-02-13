5e6fc10f1b9d30f0a0d350f3a1f900f9364db7b2
ThanhNQ <thanhnq@famasoftware.com>
2025-02-13 13:32:48 +0700

    product list

--- js/admin.js
+++ js/admin.js
@@ -66,36 +66,48 @@ $(document).ready(function(){
             } else {
                 menuBar.height(0); // Thu gọn
             }
+        });
+    });
+});
+//#region product
+	document.querySelectorAll('.btn-toggle').forEach(button => {
+		let targetID = button.getAttribute('data-bs-target');
+		let target = document.querySelector(targetID);
+		let icon = button.querySelector('.toggle-icon');
+
+		// Khi danh mục mở ra -> đổi icon thành "-"
+		target.addEventListener('shown.bs.collapse', function () {
+			icon.classList.remove('fi-rr-plus');
+			icon.classList.add('fi-rr-minus');
 		});
 
-		$(".open-menu").click(function () {
-            let menuBar = $("#menu-side");
-            if (menuBar.width() === 0) {
-                menuBar.width("100vw"); // Mở rộng
-            } else {
-                menuBar.width(0); // Thu gọn
-            }
+		// Khi danh mục đóng lại -> đổi icon thành "+"
+		target.addEventListener('hidden.bs.collapse', function () {
+			icon.classList.remove('fi-rr-minus');
+			icon.classList.add('fi-rr-plus');
 		});
 	});
-	
-	$('.centered-slide').slick({
-		centerMode: true, // Bật chế độ center
-		centerPadding: '60px', // Khoảng cách giữa các item
-		slidesToShow: 3, // Số item hiển thị
-		autoplay: true,
-		arrows: false,
-		speed: 500,
-		responsive: [
-		  {
-			breakpoint: 768,
-			settings: {
-			  arrows: false,
-			  centerMode: true,
-			  centerPadding: '40px',
-			  slidesToShow: 1
-			}
-		  }
-		]
-	});
-	
-});
+	function decreaseQuantity(idCounter) {
+		let quantityInput = document.getElementById(`${idCounter}`);
+		let quantity = parseInt(quantityInput.value);
+		if (quantity > 1) {
+			quantityInput.value = quantity - 1;
+		}
+	}
+	function increaseQuantity(idCounter) {
+		let quantityInput = document.getElementById(`${idCounter}`);
+		let quantity = parseInt(quantityInput.value);
+		quantityInput.value = quantity + 1;
+	}
+
+	const priceRange = document.getElementById("priceRange");
+	const priceValue = document.getElementById("priceValue");
+
+	function updatePrice() {
+		let price = parseInt(priceRange.value);
+		priceValue.textContent = '0 VND - ' + new Intl.NumberFormat("vi-VN").format(price) + " VND";
+	}
+
+	priceRange.addEventListener("input", updatePrice);
+	updatePrice();
+//#endregion
